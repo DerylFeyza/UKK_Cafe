c995431cc47ffeb50a8f068c223dc42e1db20160
Deryl Feyza <muhammadderylfeyza@gmail.com>
2024-09-26 13:10:05 +0700

    feat: meja zod validation

--- src/lib/validator/meja.ts
+++ src/lib/validator/meja.ts
@@ -4,5 +4,5 @@ export const MejaFormSchema = z.object({
 	nomor_meja: z
 		.string()
 		.min(1, { message: "Nomor Meja harus diisi!" })
-		.max(3, { message: "Nomor Meja maximal 3 karakter!" }),
+		.max(4, { message: "Nomor Meja maximal 3 karakter!" }),
 });
